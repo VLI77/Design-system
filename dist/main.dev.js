71ecc1de91e9ddf917e9a14a4f60f5f9f829e57d
VLI77 <vincent.li@hetic.net>
2020-10-25 00:09:26 +0200

    resize

--- dist/main.dev.js
+++ dist/main.dev.js
@@ -32,3 +32,12 @@ function change2() {
     button_register.style.borderBottom = "none";
   }
 }
+
+$(function () {
+  var w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+  var h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+  $("html, body").css({
+    "width": w,
+    "height": h
+  });
+});
