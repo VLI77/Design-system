19e912e6f96bcf87984d59df527925e9715aed14
VLI77 <vincent.li@hetic.net>
2020-10-25 00:05:42 +0200

    d

--- main.js
+++ main.js
@@ -32,3 +32,9 @@ function change2(){
     }
 }
 
+
+$(function() {
+    var w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+    var h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+    $("html, body").css({"width":w,"height":h});
+});
