717a25074bf8614570ca956f08915282dac50383
VLI77 <vincent.li@hetic.net>
2020-10-25 13:04:25 +0100

    update

--- main.js
+++ main.js
@@ -33,8 +33,3 @@ function change2(){
 }
 
 
-$(function() {
-    var w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
-    var h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
-    $("html, body").css({"width":w,"height":h});
-});
